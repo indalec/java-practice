ec2d96f565ce1c90713339245eab4fb9f5b7709c
indaleci <indaleci4@gmail.com>
2024-03-20 11:01:31 +0100

    small corrections

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-       /* for(int i = 0; i<=9; i++){
+//1 1 1 1 1 1 1 1 1
+        for(int i = 0; i<=9; i++){
             int x = 1;
-            System.out.print(x); */
+            System.out.print(x + " ");
 
             
 
