150a02bc55c93eff9c96da0d0afd333d2183d353
indaleci <indaleci4@gmail.com>
2024-03-20 09:47:26 +0100

    set ex0 as a comment

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i<=9; i++){
+       /* for(int i = 0; i<=9; i++){
             int x = 1;
-            System.out.print(x);
+            System.out.print(x); */
 
             
 
