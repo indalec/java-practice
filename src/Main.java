a27292a121f2d6f2d1837bd71e8040ab9866f9f6
indaleci <indaleci4@gmail.com>
2024-03-20 10:21:22 +0100

    exercise 6 done

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,17 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
-            
 
+        /*Task 6:
+3
+6
+9
+12
+15*/
+
+        for (int i = 3; i <= 15; i += 3) {
+
+            System.out.println(i);
         }
-
-        ;
     }
 }
