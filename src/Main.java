42b90368c1d7e4d18540a57a15847da98e5c0b11
indaleci <indaleci4@gmail.com>
2024-03-20 09:36:00 +0100

    first exercice

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        
-        System.out.println("111111111");
+        for(int i = 0; i<=9; i++){
+            int x = 1;
+            System.out.print(x);
+
+            
+
+        }
+
+        ;
     }
 }
