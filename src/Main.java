d92c5121b2813ae36ce69ce94fc30a854379deb4
indaleci <indaleci4@gmail.com>
2024-03-20 14:26:51 +0100

    exercise 14 done

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,20 @@ public class Main {
        /* for(int i = 0; i<=9; i++){
             int x = 1;
             System.out.print(x); */
+/* Task 14:
+reverse("myself") ->
+f
+l
+e
+s
+y
+m*/
 
-    }
-}
+                String original = "myself";
+                for (int i = original.length() - 1; i >= 0; i--) {
+                    System.out.println(original.charAt(i));
+                }
+            }
+        }
+
+
