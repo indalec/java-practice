b9f178b8de86b726fa7c506161b17034da570545
indaleci <indaleci4@gmail.com>
2024-03-20 11:03:36 +0100

    small corrections

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
             int x = 1;
             System.out.print(x); }*/
 
-        //123456789
+        //1 2 3 4 5 6 7 8 9
 
         for (int i = 1; i <= 9; i++) {
-            System.out.print(i); }
+            System.out.print(i + " "); }
 
     }
 }
