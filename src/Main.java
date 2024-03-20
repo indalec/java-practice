55b4b23db3d0bbc414632a61cc5eb4b6404e3be7
indaleci <indaleci4@gmail.com>
2024-03-20 11:14:54 +0100

    exercise pushed on wrong branch corrected

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,5 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
-        for (int i = 1; i <= 4; i++) {
-            if (i < 2) {
-                System.out.println("There is " + i + " cup on the table.");
-            } else {
-                System.out.println("There are " + i + " cups on the table.");
-
-            }
-
-
-        }
-
-        ;
     }
 }
