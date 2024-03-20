dd5d602fb88983dae40f651d13a9a63bfa34e1a8
indaleci <indaleci4@gmail.com>
2024-03-20 09:43:05 +0100

    exercise 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        for(int i = 0; i<=9; i++){
+        /*for (int i = 0; i <= 9; i++) {
             int x = 1;
-            System.out.print(x);
+            System.out.print(x); }*/
 
-            
+        //123456789
 
-        }
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i); }
 
-        ;
     }
 }
