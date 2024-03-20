eef85d463457e473224755cd8d6183235573fe67
indaleci <indaleci4@gmail.com>
2024-03-20 10:03:24 +0100

    exercise 3 done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
-            
+        //1 1 1 2 2 2 3 3 3 4 4 4
+
+        for(int i = 1; i<=4; i++){
+            for(int j = 0; j<=2; j++){
+
+                System.out.print(i + " ");
+
+            }
+
 
         }
 
