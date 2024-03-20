d2de88925e396b9df0d0c6713091a1ffcf7dbbc0
indaleci <indaleci4@gmail.com>
2024-03-20 10:14:59 +0100

    exercise 4 done

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,21 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
-            
+        //+
+        //++
+        //+++
+        //++++
 
+        for (int i = 0; i <= 3; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("+");
+
+
+            }
+
+            System.out.println();
         }
-
-        ;
     }
 }
