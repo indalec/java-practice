cdb4e3145c7f651e712799ad4729fe985623bd32
indaleci <indaleci4@gmail.com>
2024-03-20 09:54:34 +0100

    exercise 2 done

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,12 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
+        //2 4 6 8 10
+
+        for(int i = 2; i<=10; i+= 2){
+            System.out.print(i + " ");
+
+
             
 
         }
