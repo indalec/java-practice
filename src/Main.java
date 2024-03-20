447410b621cd1ddd25e3e09156db571a4d3fe479
indaleci <indaleci4@gmail.com>
2024-03-20 14:52:47 +0100

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-       /* for(int i = 0; i<=9; i++){
-            int x = 1;
-            System.out.print(x); */
 /* Task 14:
 reverse("myself") ->
 f
