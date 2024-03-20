580c774feb5eaaade1beaeb19d310fc2a3e2eae5
indaleci <indaleci4@gmail.com>
2024-03-20 13:40:49 +0100

    exercise 12 done

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,14 @@ public class Main {
             int x = 1;
             System.out.print(x); */
 
+        String item = "cool";
+        int times = 3;
+        String[] result = new String[times]; //This is the array
+
+        for (int i = 0; i < times; i++) {
+            result[i] = item; //assignment operation: you're saying that position "i" of the array will be filled with the content of the String "item"
+            System.out.print(result[i] + " ");
+        }
+
     }
 }
